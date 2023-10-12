0229eb1b3d731ed6339d1a537c27399ded61ec97
Yaroslav Terekh <yarolslavterekh@gmail.com>
2023-10-12 14:48:34 +0300

    Add controller and endpoint for retrieving application version

---
+++ CodebridgeTest.API/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using CodebridgeTest.BL.Settings;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodebridgeTest.API.Controllers;
+
+//[Route("api/[controller]")]
+[ApiController]
+public class HomeController : ControllerBase
+{
+    private readonly ApplicationInfo _applicationInfo;
+
+    public HomeController(ApplicationInfo applicationInfo)
+    {
+        _applicationInfo = applicationInfo;
+    }
+
+    [HttpGet("ping")]
+    public IActionResult GetApplicationVersion()
+    {
+        return Ok(_applicationInfo.ToString());
+    }
+}
---
+++ CodebridgeTest.BL/Settings/ApplicationInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodebridgeTest.BL.Settings;
+
+public class ApplicationInfo
+{
+    public string ApplicationName { get; set; }
+
+    public string ApplicationVersion { get; set; }
+
+    public override string ToString()
+    {
+        return String.Concat(ApplicationName, ".Version", ApplicationVersion);
+    }
+}
